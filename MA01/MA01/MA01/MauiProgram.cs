295a18f7a69918293f36e92ddf8f4f25d49306e2
Vulcan <vulcan.lee@gmail.com>
2022-08-06 14:10:15 +0800

    MA01|建立 MAUI 專案，加入統計圖套件，顯示圖形|練習加入其他套件或者檢視控制項

---
+++ MA01/MA01/MA01/MauiProgram.cs
@@ -0,0 +1,21 @@
+﻿using SkiaSharp.Views.Maui.Controls.Hosting;
+
+namespace MA01;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UsePrismApp<App>(PrismStartup.Configure)
+            .UseSkiaSharp(true)
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        return builder.Build();
+    }
+}
---
+++ MA01/MA01/MA01/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace MA01.ViewModels;
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using Prism.Events;
+using Prism.Navigation;
+using Prism.Services;
+public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private readonly INavigationService navigationService;
+    public ObservableCollection<ISeries> Series { get; set; } =
+        new ObservableCollection<ISeries>();
+    public MainPageViewModel(INavigationService navigationService)
+    {
+        this.navigationService = navigationService;
+
+    }
+
+    public void OnNavigatedFrom(INavigationParameters parameters)
+    {
+    }
+
+    public void OnNavigatedTo(INavigationParameters parameters)
+    {
+        Series.Add(new PieSeries<double> { Values = new double[] { 2 } });
+        Series.Add(new PieSeries<double> { Values = new double[] { 4 } });
+        Series.Add(new PieSeries<double> { Values = new double[] { 1 } });
+        Series.Add(new PieSeries<double> { Values = new double[] { 4 } });
+        Series.Add(new PieSeries<double> { Values = new double[] { 3 } });
+    }
+
+}
