15573fd13d0334c5b6162142c748829b3069c420
Vulcan <vulcan.lee@gmail.com>
2022-08-09 13:51:41 +0800

    MA06|Hello World 3 設計具有屬性變更的基底類別|採用基底類別的設計，簡化屬性變更程式設計的複雜度

---
+++ MA06/MA06/BindableBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MA06
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        #region 實作出 INotifyPropertyChanged 的程式碼
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // 使用表示式的方式來設計
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args) 
+            => PropertyChanged?.Invoke(this, args);
+        #endregion
+
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            // 先比對新舊的物件值是否相同，若不同，才會觸發 屬性變更 的通知事件
+            if (EqualityComparer<T>.Default.Equals(storage, value)) return false;
+
+            // 將此次設定的物件值，指派給該欄位
+            storage = value;
+            // 觸發 屬性變更 的通知事件
+            RaisePropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MA06/MA06/MainPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MA06
+{
+    public class MainPageViewModel : BindableBase
+    {
+        #region 針對每個具有 PropertyChanged 的屬性，都需要有底下的程式碼設計方式
+
+        #region 姓氏 屬性
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                SetProperty(ref lastName, value);
+                RaisePropertyChanged(nameof(SayHello));
+            }
+        }
+        #endregion
+
+        #region 名字 屬性
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                SetProperty(ref firstName, value);
+                RaisePropertyChanged(nameof(SayHello));
+            }
+        }
+        #endregion
+        #endregion
+
+        #region 全名 屬性
+        // 使用運算式主體定義來實作屬性 get 和 set 存取子
+        public string SayHello => $"你好 {LastName} {FirstName}";
+        #endregion
+
+    }
+}
