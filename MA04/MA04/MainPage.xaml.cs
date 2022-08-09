1cfb0fe00551d4f79a12e929af9f005bff802c5c
Vulcan <vulcan.lee@gmail.com>
2022-08-09 12:12:02 +0800

    MA04|Hello World 1 採用 Code Behind|體會沒有 MVVM 設計模式所存在的缺點

---
+++ MA04/MA04/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace MA04;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void btnSayHello_Clicked(object sender, EventArgs e)
+	{
+		#region 進行資料完整性檢查
+		if (string.IsNullOrEmpty(entry姓氏.Text))
+		{
+			DisplayAlert("警告", "必須要輸入 姓氏", "了解");
+			return;
+		}
+		if (string.IsNullOrEmpty(entry名字.Text))
+		{
+			DisplayAlert("警告", "必須要輸入 名字", "了解");
+			return ;
+		}
+		#endregion
+
+		#region 執行觸發這個按鈕要執行的工作
+		// 取得 UI 中使用者輸入文字的屬性內容
+		var 姓氏 = entry姓氏.Text;
+        // 取得 UI 中使用者輸入文字的屬性內容
+        var 名字 = entry名字.Text;
+		// 設定 文字標籤 UI 要顯示的新文字內容
+        labelHello.Text = $"你好 {姓氏} {名字} {DateTime.Now}";
+		#endregion
+    }
+}
+
--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 |MA01|建立 MAUI 專案，加入統計圖套件，顯示圖形|練習加入其他套件或者檢視控制項|
 |MA02|使用 Code Behind 取得裝置畫素與設計尺寸|學習與理解 Code Behind 用法、知道設計尺寸的意義|
 |MA03|使用點矩陣與SVG圖形，觀察放大效果|了解個平台對於圖片資源的使用與處理方式，並且學會如何加入圖片資源到專案內|
-|MA04|Hello World 1 自行實作屬性變更|純粹手作每個類別的 INotifyPropertyChanged 介面|
-|MA05|||
-|MA06|||
-|MA07|||
-|MA08|||
+|MA04|Hello World 1 採用 Code Behind|體會沒有 MVVM 設計模式所存在的缺點|
+|MA05|Hello World 2 自行實作屬性變更|純粹手作每個類別的 INotifyPropertyChanged 介面|
+|MA06|Hello World 3 設計具有屬性變更的基底類別|採用基底類別的設計，簡化屬性變更程式設計的複雜度|
+|MA07|Hello World 4 使用 PropertyChanged.Fody|體驗 PropertyChanged.Fody 來實作資料綁定程式設計，觀察其使用效果|
+|MA08|Hello World 5 CommunityToolkit.MVVM||
 |MA09|||
 |MA10|||
 |MA11|||
