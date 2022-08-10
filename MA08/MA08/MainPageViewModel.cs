c6516df0f8d1e2752ac9b5141f7f177927269418
Vulcan <vulcan.lee@gmail.com>
2022-08-10 11:34:43 +0800

    MA08|Hello World 5 CommunityToolkit.MVVM|CommunityToolkit.MVVM 是由微軟在維護的一個開源套件，觀察其在 .NET MAUI 上使用效果

--- MA08/MA08/MainPageViewModel.cs
+++ MA08/MA08/MainPageViewModel.cs
@@ -1,29 +1,43 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MA08
 {
     public partial class MainPageViewModel : ObservableObject
     {
+        // 可用於 Data Binding 中使用的命令物件
+        [RelayCommand]
+        void GenerateSayHello()
+        {
+            SayHello = $"你好 {LastName} {FirstName} {DateTime.Now}";
+        }
+
         #region 姓氏 屬性
         [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(SayHello))]
+        [NotifyPropertyChangedFor(nameof(FullName))]
         string lastName;
         #endregion
 
         #region 名字 屬性
         [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(SayHello))]
+        [NotifyPropertyChangedFor(nameof(FullName))]
         string firstName;
+        #endregion
+
+        #region 名字 屬性
+        [ObservableProperty]
+        string sayHello;
         #endregion
 
         #region 全名 屬性
         // 使用運算式主體定義來實作屬性 get 和 set 存取子
-        public string SayHello => $"你好 {LastName} {FirstName}";
+        public string FullName => $"{LastName} {FirstName}";
         #endregion
     }
 }
