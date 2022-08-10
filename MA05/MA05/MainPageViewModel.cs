554e62b2af38638608b31399a4cea894a521b76e
Vulcan <vulcan.lee@gmail.com>
2022-08-10 10:45:47 +0800

    MA05|Hello World 2 自行實作屬性變更|純粹手作每個類別的 INotifyPropertyChanged 介面

--- MA05/MA05/MainPageViewModel.cs
+++ MA05/MA05/MainPageViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MA05
 {
@@ -19,8 +20,18 @@ namespace MA05
         }
         #endregion
 
-        #region 使用 含有支援欄位的屬性 來設計需要綁定的屬性
+        // 可用於 Data Binding 中使用的命令物件
+        public ICommand SayHelloCommand { get; set; }
+        public MainPageViewModel()
+        {
+            // 傳入委派方法，為這個命令物件做初始化設定
+            SayHelloCommand = new Command(() =>
+            {
+                SayHello = $"你好 {LastName} {FirstName} {DateTime.Now}";
+            });
+        }
 
+        #region 使用 含有支援欄位的屬性 來設計需要綁定的屬性
         #region 姓氏 屬性
         private string lastName;
 
@@ -36,7 +47,7 @@ namespace MA05
                     // 發出本身屬性有發生屬性值異動，要產生通知事件
                     OnPropertyChanged("LastName");
                     // 同步發出該屬性也有發生變更的事件通知
-                    OnPropertyChanged("SayHello");
+                    OnPropertyChanged("FullName");
                 }
             }
         }
@@ -58,16 +69,36 @@ namespace MA05
                     // 發出本身屬性有發生屬性值異動，要產生通知事件
                     OnPropertyChanged("LastName");
                     // 同步發出該屬性也有發生變更的事件通知
-                    OnPropertyChanged("SayHello");
+                    OnPropertyChanged("FullName");
                 }
             }
         }
         #endregion
 
+        #region 問安文字 屬性
+        private string sayHello;
+
+        public string SayHello
+        {
+            get { return sayHello; }
+            set
+            {
+                if (value == null) return;
+                if (value.Equals(sayHello) == false)
+                {
+                    sayHello = value;
+                    // 發出本身屬性有發生屬性值異動，要產生通知事件
+                    OnPropertyChanged("SayHello");
+                }
+            }
+        }
+
+        #endregion
+        #endregion
+
         #region 全名 屬性
         // 使用運算式主體定義來實作屬性 get 和 set 存取子
-        public string SayHello => $"你好 {LastName} {FirstName}";
-        #endregion
+        public string FullName => $"你好 {LastName} {FirstName}";
         #endregion
 
     }
