8b3634f06deb5ccb3ac1bcbda8c339d38756facb
Vulcan <vulcan.lee@gmail.com>
2022-08-10 10:58:08 +0800

    MA04|Hello World 1 採用 Code Behind|體會沒有 MVVM 設計模式所存在的缺點

--- MA04/MA04/MainPage.xaml.cs
+++ MA04/MA04/MainPage.xaml.cs
@@ -29,9 +29,19 @@ public partial class MainPage : ContentPage
 		var 姓氏 = entry姓氏.Text;
         // 取得 UI 中使用者輸入文字的屬性內容
         var 名字 = entry名字.Text;
-		// 設定 文字標籤 UI 要顯示的新文字內容
-        labelHello.Text = $"你好 {姓氏} {名字} {DateTime.Now}";
+        // 設定 文字標籤 UI 要顯示的新文字內容
+        labelSayHello.Text = $"你好 {姓氏} {名字} {DateTime.Now}";
 		#endregion
+    }
+
+	private void entry姓氏_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		labelFullName.Text = $"{entry姓氏.Text} {entry名字.Text}";
+    }
+
+	private void entry名字_TextChanged(object sender, TextChangedEventArgs e)
+	{
+        labelFullName.Text = $"{entry姓氏.Text} {entry名字.Text}";
     }
 }
 
