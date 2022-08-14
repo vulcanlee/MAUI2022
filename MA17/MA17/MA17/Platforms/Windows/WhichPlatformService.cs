fbfec45aef4dd2b504bdc4f2486ba352ba4f9637
Vulcan <vulcan.lee@gmail.com>
2022-08-14 14:00:58 +0800

    MA17|呼叫平臺程式碼練習 2 - 部分類別和方法|設計讓跨平台程式碼，可以呼叫各特定平臺 API，在此顯示實作平台名稱

---
+++ MA17/MA17/MA17/Platforms/Windows/WhichPlatformService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MA17.Services
+{
+    public partial class WhichPlatformService
+    {
+        public partial async Task<string> GetPlatformName()
+        {
+            // 故意模擬非同步作業，要 1 秒鐘的時間
+            await Task.Delay(1000);
+            return "WinUI3";
+        }
+
+    }
+}
---
+++ MA17/MA17/MA17/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace MA17.ViewModels;
+
+using System.ComponentModel;
+using System.Threading.Tasks;
+using MA17.Services;
+using Prism.Events;
+using Prism.Navigation;
+using Prism.Services;
+public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private readonly INavigationService navigationService;
+
+    public string Message { get; set; }
+
+    public MainPageViewModel(INavigationService navigationService)
+    {
+        this.navigationService = navigationService;
+
+    }
+
+    public void OnNavigatedFrom(INavigationParameters parameters)
+    {
+    }
+
+    public async void OnNavigatedTo(INavigationParameters parameters)
+    {
+        WhichPlatformService whichPlatformService = new WhichPlatformService();
+
+        string whichPlatform = await whichPlatformService.GetPlatformName();
+        Message = $"你在 {whichPlatform} 平台";
+    }
+}
