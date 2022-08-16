41063b76bf4789533dfed28b80640343ef54483c
Vulcan <vulcan.lee@gmail.com>
2022-08-16 11:15:53 +0800

    MA21|使用 XAML 標記來宣告 導航 Navigation|直接使用 XAML 標記宣告語言，標示要進行導航的目的地

---
+++ MA21/MA21/MA21/PrismStartup.cs
@@ -0,0 +1,22 @@
+﻿using MA21.ViewModels;
+using MA21.Views;
+
+namespace MA21;
+
+internal static class PrismStartup
+{
+    public static void Configure(PrismAppBuilder builder)
+    {
+        builder.RegisterTypes(RegisterTypes)
+                .OnAppStart("NavigationPage/MainPage");
+    }
+
+    private static void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        containerRegistry.RegisterForNavigation<MainPage>()
+                     .RegisterInstance(SemanticScreenReader.Default);
+
+        // 記得要註冊新的頁面，否則 XAML 導航宣告將會無效用
+        containerRegistry.RegisterForNavigation<NextPage, NextPageViewModel>();
+    }
+}
---
+++ MA21/MA21/MA21/Views/NextPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MA21.Views;
+
+public partial class NextPage : ContentPage
+{
+	public NextPage()
+	{
+		InitializeComponent();
+	}
+}
